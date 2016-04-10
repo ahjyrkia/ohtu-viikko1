8b1075d69b6ad40ac4733b7f7ae4e9ffe39d7660
anton <anton.jyrkiainen@helsinki.fi>
2016-04-10 20:48:13 +0300

    initial commit

---
+++ src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,65 @@
+package ohtu.services;
+
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public class AuthenticationService {
+
+    private UserDao userDao;
+
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        if (!username.matches("^([a-zA-Z]).{3,}$")) {
+            return true;
+        }
+        int count = 0;
+        if (password.matches(".*[a-z].*")) {
+            count++;
+        }
+        if (password.matches(".*[!”#$%&’()*+,./;:=?_@>-].*")) {
+            count++;
+        }
+        if (password.matches(".*[0-9].*")) {
+            count++;
+        }
+        if (password.length() >= 8) {
+            count++;
+        }
+        if (count >= 3) {
+            return false;
+        }
+        return true;
+    }
+}
